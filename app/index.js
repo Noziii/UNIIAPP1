d4adde93684e1d66e531769f1148ae930026d6ac
Fefeswork <2306258@students.wits.ac.za>
2023-11-15 23:23:59 +0200

    Final commit

--- app/index.js
+++ app/index.js
@@ -5,11 +5,11 @@ import Home from "./Home";
 import CampusEvents from "./CampusEvents";
 import Res from "./Res";
 import LocalHotspots from "./LocalHotspots";
-import RestaurantScreen from "./RestaurantScreen";
-import MatrixGo from "./MatrixGo";
-import CartScreen from "./CartScreen";
-import OrderPreparingScreen from "./OrderingPreparingScreen";
-import DeliveryScreen from "./DeliveryScreen";
+import HomeScreen from "../matrixgo/screens/HomeScreen";
+import RestaurantScreen from "../matrixgo/screens/RestaurantScreen";
+import CartScreen from "../matrixgo/screens/CartScreen";
+import OrderPreparingScreen from "../matrixgo/screens/OrderPreparingScreen";
+import DeliveryScreen from "../matrixgo/screens/DeliveryScreen";
 
 const Stack = createStackNavigator();
 
@@ -20,23 +20,11 @@ const App = () => {
       <Stack.Screen name="CampusEvents" component={CampusEvents} />
       <Stack.Screen name="Res" component={Res} />
       <Stack.Screen name="LocalHotspots" component={LocalHotspots} />
-      <Stack.Screen name="MatrixGO" component={MatrixGo} />
+      <Stack.Screen name="MatrixGo" component={HomeScreen} />
       <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-      <Stack.Screen
-        name="Cart"
-        options={{ presentation: "modal" }}
-        component={CartScreen}
-      />
-      <Stack.Screen
-        name="OrderPreparing"
-        options={{ presentation: "fullScreenModal" }}
-        component={OrderPreparingScreen}
-      />
-      <Stack.Screen
-        name="Delivery"
-        options={{ presentation: "fullScreenModal" }}
-        component={DeliveryScreen}
-      />
+      <Stack.Screen name="Cart" options={{presentation: 'modal'}} component={CartScreen} />
+              <Stack.Screen name="Order Received" options={{presentation: 'fullScreenModal'}} component={OrderPreparingScreen} />
+              <Stack.Screen name="Preparing Order" options={{presentation: 'fullScreenModal'}} component={DeliveryScreen} />
     </Stack.Navigator>
   );
 };
