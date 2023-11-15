c22e5c4acff3e3d99a918b71542c10951b157336
Noziii <100632745+Noziii@users.noreply.github.com>
2023-11-15 12:39:09 +0200

    final iterations

--- app/LocalHotspots.js
+++ app/LocalHotspots.js
@@ -7,53 +7,102 @@ import {
   FlatList,
   ScrollView,
   StyleSheet,
+  Linking,
 } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 
 const data = [
   {
     id: "1",
-    name: "Restaurant 1",
-    distance: "2 miles",
-    rating: "4.5",
-    type: "Italian",
-    image: require("./images/Kitcheners.jpg"),
+    name: "The Wing Republic",
+    rating: "4.2  (1,3k)",
+    type: "Bar",
+    distance: "6 De Beer St, Braamfontein",
+    image: require("./images/Wingrepublic.jpg"),
+    website: "https://thewingrepublic.co.za/",
   },
   {
     id: "2",
-    name: "Restaurant 2",
-    distance: "3 miles",
-    rating: "4.0",
-    type: "Mexican",
+    name: "The Playground",
+    rating: "4.0  (1,2k)",
+    type: "Market",
+    distance: "73 Juta St, Braamfontein",
     image: require("./images/ThePlayground.jpg"),
+    website: "https://www.theplayground.co.za/",
   },
-  // Add more data as needed
+
+  {
+    id: "3",
+    name: "Saigon Suzy",
+    rating: "4.3  (1,5k)",
+    type: "Vietnamese restaurant",
+    distance: "144 Jan Smuts  Ave, Parkwood",
+    image: require("./images/Suzy.jpg"),
+    website: "https://saigonsuzy.co.za/",
+  },
+
+  {
+    id: "4",
+    name: "Uncle Faouzi",
+    rating: "4.0 (1,1k)",
+    type: "Fast food restaurant",
+    distance: "73 Juta St, Braamfontein",
+    image: require("./images/Uncle.jpg"),
+    website: "https://unclefaouzi.co.za/",
+  },
+
+  {
+    id: "5",
+    name: "Sumo Nightclub",
+    rating: "4.0 (1k)",
+    type: "Night club",
+    distance: "160 Jan Smuts Ave, Rosebank",
+    image: require("./images/Sumo.jpg"),
+    website: "https://sumoclub.co.za/",
+  },
 ];
 
 const iconData = [
-  { id: "1", icon: "pizza" },
-  { id: "2", icon: "wine" },
-  { id: "3", icon: "beer" },
-  { id: "4", icon: "fast-food" },
-  // Add more icons as needed
+  { id: "1", icon: "pizza", label: "Pizza", bgColor: "lightblue" },
+  { id: "2", icon: "wine", label: "Wine", bgColor: "lightgreen" },
+  { id: "3", icon: "beer", label: "Beer", bgColor: "lightcoral" },
+  { id: "4", icon: "fast-food", label: "Burgers", bgColor: "lightsalmon" },
+  { id: "5", icon: "ice-cream", label: "Desert", bgColor: "lightcyan" },
+  {
+    id: "6",
+    icon: "ios-musical-notes",
+    label: "Groove",
+    bgColor: "lightyellow",
+  },
 ];
 
 class Card extends Component {
+  handleCardPress = () => {
+    const { item } = this.props;
+    if (item.website) {
+      Linking.openURL(item.website);
+    }
+  };
   render() {
     const { item } = this.props;
     return (
-      <View style={styles.card}>
+      <TouchableOpacity onPress={this.handleCardPress} style={styles.card}>
         <Image source={item.image} style={styles.cardImage} />
         <TouchableOpacity style={styles.heartIcon}>
           <Icon name="heart" size={20} color="red" />
         </TouchableOpacity>
         <View style={styles.cardDetails}>
           <Text style={styles.cardTitle}>{item.name}</Text>
-          <Text>{item.distance}</Text>
-          <Text>Rating: {item.rating}</Text>
-          <Text>Type: {item.type}</Text>
+          <Text>
+            {item.rating} <Icon name="star" size={15} color="orange" />
+          </Text>
+          <Text> {item.type}</Text>
+          <Text>
+            <Icon name="location" size={15} color="grey" />
+            {item.distance}
+          </Text>
         </View>
-      </View>
+      </TouchableOpacity>
     );
   }
 }
@@ -62,7 +111,8 @@ class IconCard extends Component {
   render() {
     const { item } = this.props;
     return (
-      <View style={styles.iconCard}>
+      <View style={[styles.iconCard, { backgroundColor: item.bgColor }]}>
+        <Text style={styles.iconLabel}>{item.label}</Text>
         <Icon name={item.icon} size={30} color="black" />
       </View>
     );
@@ -73,20 +123,18 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <FlatList
-            data={iconData}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => <IconCard item={item} />}
-            horizontal
-          />
-        </ScrollView>
+        <View style={styles.iconContainer}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {iconData.map((item) => (
+              <IconCard key={item.id} item={item} />
+            ))}
+          </ScrollView>
+        </View>
 
         <FlatList
           data={data}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => <Card item={item} />}
-          vertical
         />
       </View>
     );
@@ -101,11 +149,13 @@ const styles = StyleSheet.create({
   card: {
     borderRadius: 10,
     overflow: "hidden",
-    marginBottom: 10,
+    marginBottom: 20,
+    marginLeft: 30,
+    marginRight: 30,
   },
   cardImage: {
     width: "100%",
-    height: 200,
+    height: 150,
     resizeMode: "cover",
   },
   heartIcon: {
@@ -118,18 +168,43 @@ const styles = StyleSheet.create({
   },
   cardDetails: {
     padding: 10,
-    backgroundColor: "white",
+    backgroundColor: "#eeeeee",
   },
   cardTitle: {
     fontSize: 18,
     fontWeight: "bold",
     marginBottom: 5,
   },
+  iconContainer: {
+    marginBottom: 25,
+    marginTop: 25,
+    marginLeft: 20,
+    backgroundColor: "#e7e7e7",
+    padding: 10,
+    margin: 5,
+    borderRadius: 10,
+    alignItems: "center",
+    shadowColor: "#000",
+  },
   iconCard: {
     backgroundColor: "lightgray",
     padding: 10,
     margin: 5,
     borderRadius: 10,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.75,
+    shadowRadius: 5,
+    elevation: 9,
+  },
+  iconLabel: {
+    fontSize: 12,
+    fontWeight: "bold",
+    marginBottom: 5,
   },
 });
 
